9ac03b0111674950656bb252f26199f3700d428f
ywendy <yjian84@163.com>
2015-08-21 00:42:52 +0800

    dingshi任务

---
+++ wendy-quartz/src/main/java/com/snailxr/base/task/TestTaskMy.java
@@ -0,0 +1,13 @@
+package com.snailxr.base.task;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTaskMy {
+
+	
+	public void test(){
+		System.out.println("自定义任务被触发了");
+	}
+	
+}
